1151fc8efceb8ed11079309c6f643014fcb14a2c
Omar Amjad <omaramjad54@gmail.com>
2024-12-06 19:23:44 +0200

    final

--- assets/js/category.js
+++ assets/js/category.js
@@ -3,18 +3,21 @@ const getProducts = async()=>{
    const params= new URLSearchParams(window.location.search).get('category');
    console.log(params)
     const {data} = await axios.get(`https://dummyjson.com/products/category/${params}`)
+    console.log(data)
     return data
 }
 const showProducts = async () =>{
+  document.querySelector(".loader-container").classList.add("active")
     const product  = await getProducts();
     document.querySelector(".products .container .row").innerHTML=product.products.map ((product)=>{
         return `
-        <div class = product>
+        <a class = product href="details.html?id=${product.id}">
         <img src="${product.thumbnail}"/>
         <h2>${product.title}</h2>
         <p>${product.price}</p>
-        </div>
+        </a>
       `  
     }).join("");
+    document.querySelector(".loader-container").classList.remove("active")
 }
 showProducts();
